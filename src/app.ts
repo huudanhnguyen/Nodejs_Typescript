a0ea8e1bee96168e9f9c168a20511c5ad9534544
Danh <huudanh2001.hd@gmail.com>
2025-11-10 14:33:56 +0700

    add nodemon, ejs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-const express= require("express");
-const app=express();
-const PORT=8080;
+const express = require("express");
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
+//config view engine
+app.set('view engine','ejs');
+app.set('views',__dirname +'/views');
+
+app.get("/", (req, res) => {
+  res.render("home.ejs")
+});
+
 app.listen(PORT, () => {
   console.log(`my app is running on port:${PORT}`);
 });
