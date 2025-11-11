6e7b14441168da888b824c472b4524691803a41c
Danh <huudanh2001.hd@gmail.com>
2025-11-11 12:13:06 +0700

    connect My SQl

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 const express = require("express");
 import "dotenv/config";
 import webRoutes from "./routes/web";
+import getConnection from "./config/database";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -8,15 +9,14 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //config view engine
-app.set('view engine','ejs');
-app.set('views',__dirname +'/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 //config router
 webRoutes(app);
 //confit static files
 app.use(express.static("public"));
-
+getConnection();
 app.listen(PORT, () => {
   console.log(`my app is running on port:${PORT}`);
 });
