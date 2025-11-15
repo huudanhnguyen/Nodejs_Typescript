80fff089b5fbe4e72c0900e7c7396edafb7f5401
Danh <huudanh2001.hd@gmail.com>
2025-11-15 21:18:59 +0700

    setup prisma

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage);
-  router.get("/create-user", getCreateUserPage);
+  router.get("/user/create", getCreateUserPage);
   router.post("/handle-create-user", postCreateUserPage);
   router.post("/users/delete/:id", deleteUser);
   router.get("/users/edit/:id", getEditUserPage);
